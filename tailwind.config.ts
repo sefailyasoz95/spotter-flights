9492c4fe4c6cea8c14723fe1d4e4244bfecc7656
Sefa İlyas Öz <sefailyas1455@gmail.com>
2025-02-02 22:24:14 +0300

    initial set up

---
+++ tailwind.config.ts
@@ -0,0 +1,65 @@
+import type { Config } from "tailwindcss";
+
+export default {
+	darkMode: "class",
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				radius: "var(--radius)",
+			},
+			keyframes: {
+				float: {
+					"0%, 100%": {
+						transform: "translateY(0) rotate(var(--tw-rotate))",
+					},
+					"50%": {
+						transform: "translateY(-20px) rotate(var(--tw-rotate))",
+					},
+				},
+			},
+			animation: {
+				float: "float 6s ease-in-out infinite",
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config;
